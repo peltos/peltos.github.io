ec4119c959d0308d9f180f78825cd9647837fa51
Ron Pelt <pelt8@hotmail.com>
2021-04-23 00:01:02 +0200

    more changes

--- d20d/Scripts/gh-pages-deploy.js
+++ d20d/Scripts/gh-pages-deploy.js
@@ -3,14 +3,21 @@ const execa = require("execa");
 const fs = require("fs");
 (async () => {
   try {
+    console.log("building app...");
     await execa("ng", ["build", "--base-href", "https://peltos.github.io/d20d/"]);
+    console.log("Changing branch to 'gh-pages'...");
     await execa("git", ["checkout", "gh-pages"]);
+    console.log("Changing files...");
     await execa("find", ["./", "-maxdepth", "1", "-type", "f", "-exec", "rm", "-iv", "-f", "{}\;"]);
-    await execa("mv", ["-v", "dist/*", "."]);
+    await execa("cd", ["dist"]);
+    await execa("mv", ["-v", "*", "../"]);
+    await execa("cd", ["../"]);
     await execa("rm", ["-r", "dist"]);
+    console.log("Committing new files...");
     await execa("find", ["./", "-maxdepth", "1", "-type", "f", "-exec", "git", "add", "{}\;"]);
     await execa("git", ["commit", "-m", "\"gh-pages changes\""]);
     await execa("git", ["push"]);
+    console.log("Done!!");
   } catch (e) {
     // eslint-disable-next-line no-console
     console.log(e.message);
